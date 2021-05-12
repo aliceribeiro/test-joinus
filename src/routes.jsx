c836860f4a9d1aa181f64fa7afe8ad064c002b6e
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-05-12 15:27:40 -0300

    feat: atualizando o endereço das páginas

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,9 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
-import Login from './pages/Login';
-import PizzaDetail from './pages/PizzaDetail';
-import PizzaList from './pages/PizzaList';
-import CheckOut from './pages/CheckOut';
-
+import Login from "./pages/Login/Login";
+import PizzaDetail from "./pages/PizzaDetail/PizzaDetail";
+import PizzaList from "./pages/PizzaList/PizzaList";
+import CheckOut from "./pages/CheckOut/CheckOut";
 
 function Routes() {
   return (
@@ -13,7 +11,7 @@ function Routes() {
       <Switch>
         <Route path="/" exact component={Login} />
         <Route path="/pizza-list" component={PizzaList} />
-        <Route path="/pizza-detail" component={PizzaDetail} />
+        <Route path="/pizza-detail/:id" component={PizzaDetail} />
         <Route path="/check-out" component={CheckOut} />
       </Switch>
     </BrowserRouter>
